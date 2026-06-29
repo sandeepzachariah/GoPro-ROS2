3071799928e819e9deb019a69136437f0df35a67
Sandeep Zachariah <sandeepzachariah123@gmail.com>
2026-06-29 17:30:14 -0400

    updated ffmpeg

--- scripts/webcam_service.sh
+++ scripts/webcam_service.sh
@@ -11,4 +11,4 @@ sudo gopro webcam -n -p enp*
 
 
 # Publish the messages on a video device
-ffmpeg -nostdin -threads 1 -i 'udp://@0.0.0.0:8554?overrun_nonfatal=1&fifo_size=50000000' -f:v mpegts -fflags nobuffer -vf format=yuv420p -f v4l2 /dev/video42
+ffmpeg -nostdin   -fflags nobuffer   -flags low_delay   -f mpegts   -i 'udp://@0.0.0.0:8554?overrun_nonfatal=1&fifo_size=500000'   -fflags nobuffer   -vf format=yuv420p   -f v4l2 /dev/video42
